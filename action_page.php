c8afb432a7830111e3727f78faffef5ad5d83e38
Alecn59 <133815073+Alecn59@users.noreply.github.com>
2023-05-16 14:57:05 -0700

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,10 +1,16 @@
-
 <?php
- $path = 'data.txt';
- if (isset($_POST['field1']) && isset($_POST['field2'])) {
-    $fh = fopen($path,"a+");
-    $string = $_POST['field1'].' - '.$_POST['field2'];
-    fwrite($fh,$string); // Write information to the file
-    fclose($fh); // Close the file
- }
-?>
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $inputText = $_POST['inputText'];
+    // Modify the file path and name as per your requirements
+    $filePath = 'path/to/your/file.txt';
+
+    // Open the file in append mode
+    $file = fopen($filePath, 'a');
+
+    // Write the user input to the file
+    fwrite($file, $inputText . "\n");
+
+    // Close the file
+    fclose($file);
+}
+?> 

bcea3bfbba0d551ec7f96912c17e6b3fdd722011
Alecn59 <133815073+Alecn59@users.noreply.github.com>
2023-05-16 14:58:17 -0700

    Update and rename action_page.php to save.php

--- save.php
+++ save.php
@@ -2,7 +2,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $inputText = $_POST['inputText'];
     // Modify the file path and name as per your requirements
-    $filePath = 'path/to/your/file.txt';
+    $filePath = 'Alecn59.github.io/file.txt';
 
     // Open the file in append mode
     $file = fopen($filePath, 'a');

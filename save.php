50cab477ba370ac11a79089e01bfb700cc249537
Alecn59 <133815073+Alecn59@users.noreply.github.com>
2023-05-17 13:34:14 -0700

    Update save.php

--- save.php
+++ save.php
@@ -1,17 +1,2 @@
 <?php
-phpinfo();
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $inputText = $_POST['inputText'];
-    // Modify the file path and name as per your requirements
-    $filePath = 'Alecn59.github.io/file.txt';
-
-    // Open the file in append mode
-    $file = fopen($filePath, 'a');
-
-    // Write the user input to the file
-    fwrite($file, $inputText . "\n");
-
-    // Close the file
-    fclose($file);
-}
-?> 
+print_r($_POST);
